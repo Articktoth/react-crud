6b4d795ff2a17467b24587abadd8c352c3d4e8cc
Alejandro Soto <tothdevelop@gmail.com>
2020-01-15 13:55:09 -0300

    Updates

---
+++ precache-manifest.cf3141ddd41d51be0d029d8341219905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ac1d08691327c769aa50db595e26bf",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "b85741c5545df4580c33",
+    "url": "/react-crud/static/css/main.7f0f5042.chunk.css"
+  },
+  {
+    "revision": "ff98d5488b19a4161e35",
+    "url": "/react-crud/static/js/2.e83b5cbc.chunk.js"
+  },
+  {
+    "revision": "507a7370b52a72b5f39c2fecbeb4dc40",
+    "url": "/react-crud/static/js/2.e83b5cbc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b85741c5545df4580c33",
+    "url": "/react-crud/static/js/main.08f00385.chunk.js"
+  },
+  {
+    "revision": "7729d9bb77faf5424e2f",
+    "url": "/react-crud/static/js/runtime-main.10619d90.js"
+  }
+]);
